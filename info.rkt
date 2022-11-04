5c63b4a7201ddbe8b22bf340055fb3e2a3580e90
a11ce <a11ce@protonmail.com>
2022-11-04 16:38:46 -0500

    p l s build

--- info.rkt
+++ info.rkt
@@ -1,6 +1,11 @@
 #lang info
 
+(define collection "gnome")
+
 (define scribblings '(("actual-real-racket.scrbl")))
 
 ; this does not apply to the gnome
 (define license '(Apache-2.0 OR MIT))
+
+(define deps '("base"))
+(define build-deps '("scribble-lib"))
